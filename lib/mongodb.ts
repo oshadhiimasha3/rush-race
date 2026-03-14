e1c46116fd0587c9cc6fbdefca0f8896061007b9
oshadhiimasha3 <oshadhiimasha3@gmail.com>
2026-03-14 14:55:11 +0530

    updated mongodb connection logic

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,35 +1,35 @@
-// import { MongoClient } from "mongodb";
+import { MongoClient } from "mongodb";
 
-// const uri = process.env.MONGODB_URI as string;
+const uri = process.env.MONGODB_URI as string;
 
-// if (!uri) {
-//   throw new Error("Please add MONGODB_URI to .env.local");
-// }
-
-// let client: MongoClient;
-// let clientPromise: Promise<MongoClient>;
-
-// const globalWithMongo = global as typeof globalThis & {
-//   _mongoClientPromise?: Promise<MongoClient>;
-// };
-
-// if (!globalWithMongo._mongoClientPromise) {
-//   client = new MongoClient(uri);
-//   globalWithMongo._mongoClientPromise = client.connect();
-// }
-
-// clientPromise = globalWithMongo._mongoClientPromise;
-
-// export default clientPromise;
-
-
-
-import mongoose from "mongoose";
-
-const MONGODB_URI = process.env.MONGODB_URI!;
-
-export async function connectDB() {
-  if (mongoose.connections[0].readyState) return;
-
-  await mongoose.connect(MONGODB_URI);
+if (!uri) {
+  throw new Error("Please add MONGODB_URI to .env.local");
 }
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+const globalWithMongo = global as typeof globalThis & {
+  _mongoClientPromise?: Promise<MongoClient>;
+};
+
+if (!globalWithMongo._mongoClientPromise) {
+  client = new MongoClient(uri);
+  globalWithMongo._mongoClientPromise = client.connect();
+}
+
+clientPromise = globalWithMongo._mongoClientPromise;
+
+export default clientPromise;
+
+
+
+// import mongoose from "mongoose";
+
+// const MONGODB_URI = process.env.MONGODB_URI!;
+
+// export async function connectDB() {
+//   if (mongoose.connections[0].readyState) return;
+
+//   await mongoose.connect(MONGODB_URI);
+// }
