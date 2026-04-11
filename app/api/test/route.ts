41135939e1ed09555d825bc7f93592a86f9c40e6
oshadhiimasha3 <oshadhiimasha3@gmail.com>
2026-04-11 13:38:19 +0530

    updated to use connectDB correctly.

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,20 +1,13 @@
-import clientPromise from "@/lib/mongodb";
+import { connectDB } from "@/lib/mongodb";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const client = await clientPromise;
-
-    const db = client.db("rushDB");
-
-    const result = await db.collection("users").insertOne({
-      name: "Test User",
-      createdAt: new Date(),
-    });
+    await connectDB();
 
     return NextResponse.json({
       success: true,
-      insertedId: result.insertedId,
+      message: "DB connection successful",
     });
   } catch (error) {
     return NextResponse.json({
